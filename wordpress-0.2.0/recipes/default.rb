708c8324b688b3b91f95575bd149aca732a9705d
unknown <waseem.rauf@WC057237.mit>
2014-09-07 18:49:19 +1000

    chaging code

--- wordpress-0.2.0/recipes/default.rb
+++ wordpress-0.2.0/recipes/default.rb
@@ -5,13 +5,16 @@
 # Copyright 2014, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
-package "php55" do
+package "php55", " php55-mysqlnd.x86_64" do
 	action :install
 end
 
 service "httpd" do
 	supports :status => true, :restart => true, :reload => true
 	action [ :enable, :start ]
+end
+package "mysql" do
+	action :install
 end
 
 remote_file "/var/www/html/wordpress-4.0-en_CA.tar.gz" do
@@ -24,6 +27,7 @@ bash "wordpress-4.0-en_CA.tar.gz" do
 	cwd "/var/www/html"
 	code <<-EOH
 	tar -zxvf wordpress-4.0-en_CA.tar.gz
+	cp wordpress/* /var/www/html
 	EOH
 end
 
