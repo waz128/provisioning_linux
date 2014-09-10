56e608693197b3de52d799b6acf728177891c47c
waseem.rauf <waseem.rauf@melbourneit.com.au>
2014-09-10 16:51:57 +1000

    test

---
+++ wordpress-0.2.0/recipes/mysql - Copy.rb
@@ -0,0 +1,33 @@
+CHARS = ('0'..'9').to_a + ('A'..'Z').to_a + ('a'..'z').to_a
+def random_password(length=10)
+	CHARS.sort_by { rand }.join[0...length]
+end
+
+puts "#{random_password}"
+
+package "mysql55-server" do
+	action :install
+end
+
+service "mysqld" do
+	supports :status => true, :restart => true, :reload => true
+	action [ :enable, :start ]
+end
+
+ruby "mysqld" do
+	user "root"
+	code <<-EOH
+	/usr/bin/mysqladmin -u root password #{random_password}	
+	EOH
+end
+
+
+file "/tmp/mysqlrootpass.txt" do
+	action :create
+	owner "root"
+	group "root"
+	mode "0644"
+	content "#{random_password}"
+end
+
+puts '#{random_password}'
