293207e9e87f870523824e5089dcbc5556f78187
waseem.rauf <waseem.rauf@melbourneit.com.au>
2014-09-10 16:55:38 +1000

    test3

--- wordpress-0.2.0/metadata.rb
+++ wordpress-0.2.0/metadata.rb
@@ -6,4 +6,4 @@ description      'Installs/Configures wordpress'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.4'
 
-depends		 'smbfs', '= 0.2.0'
+depends		 'smbfs', '= 0.4.0'
