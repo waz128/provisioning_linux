4da889fb0e3275ab0279e98b7b97269641e469ad
waseem.rauf <waseem.rauf@melbourneit.com.au>
2014-09-10 17:11:34 +1000

    test!

--- wordpress-0.2.0/recipes/sophos.rb
+++ wordpress-0.2.0/recipes/sophos.rb
@@ -8,7 +8,7 @@
 ####
 include_recipe "smbfs"
 
-Create share directory
+#Create share directory
 directory "/mnt/sophos-share" do
   owner "root"
   group "root"
@@ -19,12 +19,10 @@ end
  #Mount share depends on: https://github.com/realityforge/chef-smbfs
  include_recipe 'smbfs::attribute_driven'
 
-install sophos
+#install sophos
 bash 'install-sophos' do
   code <<-EOH
     /opt/sophos-share/CIDs/S002/savlinux/install.sh "/opt/sophos-av" install.sh --automatic --SavWebPassword= --enableSavProtectOnBoot=True --update-source-username=savaws --update-source-password="88837cm20ncmMNC-3&^@)C" --instdir=/opt/sophos-av --update-cache-path=/opt/sophos-av/update/cache/Primary --enableSavWeb=False --update-type=o --update-source-path=http://apse2-0001sec.aws.server-noc.com/SophosUpdate/CIDs/S008 --acceptlicence=True --SavWebUsername=admin --SavWebPort=8081 --enableRMS=true --automatic=True --enableOnAccess=true --update-period-minutes=60
     EOH
   not_if { Dir.exists?('/opt/sophos-av') }
 end
-
-depends on: HTTP
