1f65ecdcb8c1e9206a8a1093f8eb0c4e868f3a5c
unknown <waseem.rauf@WC057237.mit>
2014-09-11 21:05:29 +1000

    test

--- wordpress-0.2.0/recipes/os-hardening.rb
+++ wordpress-0.2.0/recipes/os-hardening.rb
@@ -1,9 +1,21 @@
-#include_attribute "wordpress.0.2.0::os-hardening"
-node.override['desktop']['enable'] = true
-node.override['network']['forwarding'] = true
-node.override['network']['ipv6']['enable'] = true
-node.override['network']['arp']['restricted'] = true
-
+node.override['desktop']['enable'] = false
+node.override['network']['forwarding'] = false
+node.override['network']['ipv6']['enable'] = false
+node.override['network']['arp']['restricted'] = false
+node.override['env']['umask'] = "027"
+node.override['env']['root_path'] = "/"
+node.override['auth']['pw_max_age'] = 90
+node.override['auth']['pw_min_age'] = 7
+node.override['auth']['retries'] = 5
+node.override['auth']['lockout_time'] = 600
+node.override['auth']['timeout'] = 60
+node.override['auth']['allow_homeless'] = true
+node.override['auth']['pam']['passwdqc']['enable'] = false
+node.override['security']['kernel']['enable_module_loading'] = true
+node.override['security']['kernel']['enable_sysrq'] = false
+node.override['security']['kernel']['enable_core_dump'] = false
+node.override['security']['suid_sgid']['enforce'] = false
+node.override['security']['packages']['clean'] = true
 
 include_recipe "os-hardening"
 
