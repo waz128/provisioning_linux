7d1cf373dfeed12c8046d17afc75b01050ef1bde
waseem.rauf <waseem.rauf@melbourneit.com.au>
2014-09-12 08:35:22 +1000

    test

--- wordpress-0.2.0/recipes/mysql.rb
+++ wordpress-0.2.0/recipes/mysql.rb
@@ -1,8 +1,3 @@
-CHARS = ('0'..'9').to_a + ('A'..'Z').to_a + ('a'..'z').to_a
-def random_password(length=10)
-  CHARS.sort_by { rand }.join[0...length]
-end
-
 package "mysql-server" do
   action :install
   version " 5.5.38-1.0"
