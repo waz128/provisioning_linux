475837b1d1927b86ab3a232f0ff38aed47978647
unknown <waseem.rauf@WC057237.mit>
2014-09-11 20:52:40 +1000

    test

--- wordpress-0.2.0/recipes/os-hardening.rb
+++ wordpress-0.2.0/recipes/os-hardening.rb
@@ -1,3 +1,9 @@
 #include_attribute "wordpress.0.2.0::os-hardening"
+node.override['desktop']['enable'] = true
+node.override['network']['forwarding'] = true
+default['network']['ipv6']['enable'] = true
+default['network']['arp']['restricted'] = true
+
+
 include_recipe "os-hardening"
 
