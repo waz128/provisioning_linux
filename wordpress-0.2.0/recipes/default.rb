ae5c1c281b76abb50cf70a53c29d04fc32bac797
waseem.rauf <waseem.rauf@melbourneit.com.au>
2014-09-11 17:56:49 +1000

    tesr3

--- wordpress-0.2.0/recipes/default.rb
+++ wordpress-0.2.0/recipes/default.rb
@@ -5,7 +5,8 @@
 # Copyright 2014, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
-include_recipe "wordpress-0.2.0::os-harden"
+include_recipe "os-hardening"
 #include_recipe "wordpress-0.2.0::sophos"
 #include_recipe "wordpress-0.2.0::apache+wordpress" 
 #include_recipe "wordpress-0.2.0::mysql"
+
--- wordpress-0.2.0/recipes/os-harden.rb
+++ wordpress-0.2.0/recipes/os-harden.rb
@@ -1,14 +1,3 @@
 include_recipe "os-hardening"
 
-{
-"chef_type": "role"
-"json_class": "Chef::Role",
-"security" : {
-  "kernel" : {
-    "enable_module_loading" : true
-  }
-}
-},
-
-
 
