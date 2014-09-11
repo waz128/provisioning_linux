82ea919b2fbd9824e9f52efa663e6ffa4d3d8956
waseem.rauf <waseem.rauf@melbourneit.com.au>
2014-09-12 08:34:15 +1000

    test

--- wordpress-0.2.0/recipes/mysql.rb
+++ wordpress-0.2.0/recipes/mysql.rb
@@ -7,14 +7,3 @@ package "mysql-server" do
   action :install
   version " 5.5.38-1.0"
 end
-
-
-file "/tmp/mysql.txt" do
-  action :create
-  path "path"
-  backup 5
-  owner "root"
-  group "root"
-  mode "0644"
-  content "#{random_password}"
-end
