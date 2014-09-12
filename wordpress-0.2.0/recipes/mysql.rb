fbe7af2de9ead38f1f9ee9b4b81844d3db5434ea
waseem.rauf <waseem.rauf@melbourneit.com.au>
2014-09-12 14:13:06 +1000

    test

--- wordpress-0.2.0/recipes/mysql.rb
+++ wordpress-0.2.0/recipes/mysql.rb
@@ -1,24 +1,25 @@
-package "mysql-server" do
-  action :install
-end
-
-service "mysqld" do
-  supports :status => true, :restart => true, :reload => true
-  action [:start ]
-end
-
-execute "assign-root-password" do
-  CHARS = ('0'..'9').to_a + ('A'..'Z').to_a + ('a'..'z').to_a
-    def self.random_password(length=10)
-      CHARS.sort_by { rand }.join[0...length]
+if platform_family?("centos", "rhel")
+    package "mysql-server" do
+      action :install
     end
-  action :run
-  command "/usr/bin/mysqladmin -u root password '#{random_password}'"
-  command "touch /tmp/mysqlpass.txt && echo '#{random_password}' > /tmp/mysqlpass.txt"
-end
 
-service "mysqld" do
-  supports :status => true, :restart => true, :reload => true
-  action [:enable]
-end
+    service "mysqld" do
+      supports :status => true, :restart => true, :reload => true
+      action [:start ]
+    end
 
+    execute "assign-root-password" do
+      CHARS = ('0'..'9').to_a + ('A'..'Z').to_a + ('a'..'z').to_a
+        def self.random_password(length=10)
+          CHARS.sort_by { rand }.join[0...length]
+        end
+      action :run
+      command "/usr/bin/mysqladmin -u root password '#{random_password}'"
+      command "touch /tmp/mysqlrootpass.txt && echo '#{random_password}' > /tmp/mysqlrootpass.txt"
+    end
+
+    service "mysqld" do
+      supports :status => true, :restart => true, :reload => true
+      action [:enable]
+    end
+end
