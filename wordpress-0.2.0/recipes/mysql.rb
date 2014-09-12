17af223eabaa754a7c76836a3e1d94caf29b1371
waseem.rauf <waseem.rauf@melbourneit.com.au>
2014-09-12 11:55:56 +1000

    test

--- wordpress-0.2.0/recipes/mysql.rb
+++ wordpress-0.2.0/recipes/mysql.rb
@@ -8,19 +8,17 @@ service "mysqld" do
 end
 
 execute "assign-root-password" do
-
   CHARS = ('0'..'9').to_a + ('A'..'Z').to_a + ('a'..'z').to_a
     def self.random_password(length=10)
       CHARS.sort_by { rand }.join[0...length]
     end
   command "/usr/bin/mysqladmin -u root password '#{random_password}'"
   action :run
+end
 
 log "write-assign-root-password" do
   message "#{random_password}"
   level :info
-end
-
 end
 
 service "mysqld" do
