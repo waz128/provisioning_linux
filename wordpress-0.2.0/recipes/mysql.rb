6e53ea5df8ba29e8d753d4a1d61db49b84ab9c17
waseem.rauf <waseem.rauf@melbourneit.com.au>
2014-09-12 15:46:18 +1000

    test

--- wordpress-0.2.0/recipes/mysql.rb
+++ wordpress-0.2.0/recipes/mysql.rb
@@ -11,7 +11,7 @@ if platform_family?("centos", "rhel")
 
     execute "assign-root-password" do
 
-      MYCONSTANT = "random_password"
+      MYCONSTANT = "#{random_password}"
       CHARS = ('0'..'9').to_a + ('A'..'Z').to_a + ('a'..'z').to_a
       def random_password(length=10)
       CHARS.sort_by { rand }.join[0...length]
