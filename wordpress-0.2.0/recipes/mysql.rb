c75d5ce8df9cee63d9a32e16172630d195ba779f
waseem.rauf <waseem.rauf@melbourneit.com.au>
2014-09-12 14:56:06 +1000

    rtet

--- wordpress-0.2.0/recipes/mysql.rb
+++ wordpress-0.2.0/recipes/mysql.rb
@@ -1,3 +1,11 @@
+$global_variable = mypass
+class myPass
+   CHARS = ('0'..'9').to_a + ('A'..'Z').to_a + ('a'..'z').to_a
+        def random_password(length=10)
+          CHARS.sort_by { rand }.join[0...length]
+        end
+end
+
 if platform_family?("centos", "rhel")
 
   package "mysql-server" do
@@ -10,12 +18,8 @@ if platform_family?("centos", "rhel")
     end
 
     execute "assign-root-password" do
-      CHARS = ('0'..'9').to_a + ('A'..'Z').to_a + ('a'..'z').to_a
-        def self.random_password(length=10)
-          CHARS.sort_by { rand }.join[0...length]
-        end
       action :run
-      command "/usr/bin/mysqladmin -u root password '#{random_password}' ; echo '#{random_password}' > /tmp/mysqlrootpass.txt"
+      command "/usr/bin/mysqladmin -u root password '#{myPass}' ; echo '#{myPass}' > /tmp/mysqlrootpass.txt"
     end
 
     service "mysqld" do
