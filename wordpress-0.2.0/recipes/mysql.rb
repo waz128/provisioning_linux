475726303a67c8ea5ddd536122ec905555c94553
waseem.rauf <waseem.rauf@melbourneit.com.au>
2014-09-12 12:55:51 +1000

    test

--- wordpress-0.2.0/recipes/mysql.rb
+++ wordpress-0.2.0/recipes/mysql.rb
@@ -7,16 +7,29 @@ service "mysqld" do
   action [:start ]
 end
 
-execute "assign-root-password" do
+bash "install something" do
   CHARS = ('0'..'9').to_a + ('A'..'Z').to_a + ('a'..'z').to_a
     def self.random_password(length=10)
       CHARS.sort_by { rand }.join[0...length]
     end
-  action :run
-  command "/usr/bin/mysqladmin -u root password '#{random_password}' >> /tmp/mysqlpass.txt"
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+  /usr/bin/mysqladmin -u root password '#{random_password}' >> mysqlpass.txt
+  EOH
 end
+
+#execute "assign-root-password" do
+  #CHARS = ('0'..'9').to_a + ('A'..'Z').to_a + ('a'..'z').to_a
+    #def self.random_password(length=10)
+      #CHARS.sort_by { rand }.join[0...length]
+    #end
+  #action :run
+  #command "/usr/bin/mysqladmin -u root password '#{random_password}' >> /tmp/mysqlpass.txt"
+#end
 
 service "mysqld" do
   supports :status => true, :restart => true, :reload => true
   action [:enable]
 end
+
