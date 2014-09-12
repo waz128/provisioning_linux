4294eefae73992adbf12593ab12163e40b6616a8
waseem.rauf <waseem.rauf@melbourneit.com.au>
2014-09-12 11:11:17 +1000

    test

--- wordpress-0.2.0/recipes/mysql.rb
+++ wordpress-0.2.0/recipes/mysql.rb
@@ -14,7 +14,7 @@ execute "assign-root-password" do
   def self.random_password(length=10)
     CHARS.sort_by { rand }.join[0...length]
   end
-  command "/usr/bin/mysqladmin -u root password '#{random_password}"
+  command "/usr/bin/mysqladmin -u root password '#{random_password}'"
   action :run
 end
 
