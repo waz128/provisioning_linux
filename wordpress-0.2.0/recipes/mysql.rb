72a38599a02a696e7a6865ac1d08a21f7379933e
unknown <waseem.rauf@WC057237.mit>
2014-09-12 18:38:22 +1000

    tes

--- wordpress-0.2.0/recipes/mysql.rb
+++ wordpress-0.2.0/recipes/mysql.rb
@@ -9,16 +9,13 @@ if platform_family?("centos", "rhel")
       action [:start ]
     end
 
-    execute "assign-root-password" do
-
-      MYCONSTANT = "#{random_password}"
-      CHARS = ('0'..'9').to_a + ('A'..'Z').to_a + ('a'..'z').to_a
-      def random_password(length=10)
-      CHARS.sort_by { rand }.join[0...length]
-      end
-      action :run
-      command "/usr/bin/mysqladmin -u root password '#{MYCONSTANT}'";
-    end
+bash "assign-root-password" do
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+  /usr/bin/mysqladmin -u root password 'genpasswd' > /tmp/mysqlroot.txt
+  EOH
+end
 
     service "mysqld" do
       supports :status => true, :restart => true, :reload => true
