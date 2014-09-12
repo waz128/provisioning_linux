0c9f98e3df5a8904117e5ba13550d2f9d82d65b8
waseem.rauf <waseem.rauf@melbourneit.com.au>
2014-09-12 13:13:33 +1000

    test

--- wordpress-0.2.0/recipes/mysql.rb
+++ wordpress-0.2.0/recipes/mysql.rb
@@ -13,8 +13,8 @@ execute "assign-root-password" do
       CHARS.sort_by { rand }.join[0...length]
     end
   action :run
-  command "/usr/bin/mysqladmin -u root password '#{random_password}' && > /tmp/test.txt"
-  command " "
+  command "/usr/bin/mysqladmin -u root password '#{random_password}'"
+  command "vim /tmp/mysqlpass.txt < #{random_password}"
 end
 
 service "mysqld" do
