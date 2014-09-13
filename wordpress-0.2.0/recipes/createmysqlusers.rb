c5eea6ce5731dfb8015bc6772ad5c17cf138d07d
unknown <waseem.rauf@WC057237.mit>
2014-09-13 19:13:31 +1000

    test

--- wordpress-0.2.0/recipes/createmysqlusers.rb
+++ wordpress-0.2.0/recipes/createmysqlusers.rb
@@ -1,5 +1,11 @@
 api_string = (0...32).map{65.+(rand(25)).chr}.join
 $mypass = "#{api_string}"
 
-print  $mypass
-print  $mypass
+print  #{api_string}
+print  #{api_string}
+
+mysql_database 'wordpress' do
+	connection(
+			:username => 'root',
+			:password => node['mysql']['#{api_string}']
+	)
