757d3a6cf9071bc0bd00f1c82e870dd07e75899a
melbit-waseem.rauf <waseem.rauf@melbourneit.com.au>
2014-09-21 21:08:53 +1000

    test

--- wordpress-0.2.0/recipes/timezone.rb
+++ wordpress-0.2.0/recipes/timezone.rb
@@ -1,9 +1,5 @@
 include_recipe "ntp"
 
-default_attributes(
-  'ntp' => {
-    'servers' => ['0.amazon.pool.ntp.org', '1.amazon.pool.ntp.org','2.amazon.pool.ntp.org', '3.amazon.pool.ntp.org', 'waztest.poo.ntp.org']
-  }
-)
+node['ntp']['servers'] = '0.amazon.pool.ntp.org', '1.amazon.pool.ntp.org','2.amazon.pool.ntp.org', '3.amazon.pool.ntp.org', 'waztest.poo.ntp.org'
 
 
