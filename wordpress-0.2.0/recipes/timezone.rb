04da1fd0dbe4b5983392acd21c2c24e4f9582f8c
melbit-waseem.rauf <waseem.rauf@melbourneit.com.au>
2014-09-21 21:02:55 +1000

    test

--- wordpress-0.2.0/recipes/timezone.rb
+++ wordpress-0.2.0/recipes/timezone.rb
@@ -1,46 +1,9 @@
-# Configure network time sync
 include_recipe "ntp"
 
-# Set system timezone - Default is Australia/Brisbane
-case node[:platform]
-  	when "ubuntu", "debian"
-		execute "update-tzdata" do
-			command "dpkg-reconfigure -f noninteractive tzdata"
-			action :nothing
-		end
+default_attributes(
+  'ntp' => {
+    'servers' => ['0.amazon.pool.ntp.org', '1.amazon.pool.ntp.org','2.amazon.pool.ntp.org', '3.amazon.pool.ntp.org', 'waztest.poo.ntp.org']
+  }
+)
 
-		file "/etc/timezone" do
-			owner "root"
-			group "root"
-			mode "00644"
-			content node[:timezone]
-  			notifies :run, "execute[update-tzdata]"
-		end
 
-		package "tzdata" do
-			action :install
-		end
-  	when "amazon", "centos", "redhat", "fedora"
-		link "/etc/localtime" do
-  			to "/usr/share/zoneinfo/#{node[:timezone]}"
-		end
-
-		template "/etc/sysconfig/clock" do
-			source "clock.erb"
-		end
-end
-
-# Logrotate
-include_recipe "logrotate"
-
-# Re-Write /etc/rc.local file
-template "/etc/rc.local" do
-	source "rc.local.erb"
-	mode 0755
-	owner 'root'
-	group 'root'
-	manage_symlink_source true
-	variables({
-		:rc_local_commands => node[:wordpress-0.2.0][:rc_local_commands]
-	})
-end
