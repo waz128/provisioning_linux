2af8d1a69db2e147720abca570d09977ba806414
melbit-waseem.rauf <waseem.rauf@melbourneit.com.au>
2014-09-21 16:55:42 +1000

    rss

--- wordpress-0.2.0/recipes/mysql.rb
+++ wordpress-0.2.0/recipes/mysql.rb
@@ -42,14 +42,18 @@ if platform_family?("centos", "rhel")
       :password => node['mysql']['server_root_password']
     }
 
+    execute "save-mysqluserpass" do
+      action :run
+      command "echo '#{mysqluser}' > /tmp/mysqluserpassword.txt"
+    end
+    
     #Create a mysql user
     mysql_database_user 'wordpress_prod' do
       connection mysql_connection_info
       password '#{mysqluser}'
       action :create
-      command "echo '#{mysqluser}' > /tmp/mysqluserpassword.txt"
       end
-    
+
     # Grant SELECT, UPDATE, and INSERT privileges to all tables in foo db from all hosts
     mysql_database_user 'wordpress_prod' do
       connection    mysql_connection_info
