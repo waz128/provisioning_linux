326717377242068746044c1fc1424402099ff432
melbit-waseem.rauf <waseem.rauf@melbourneit.com.au>
2014-09-21 17:53:08 +1000

    test

--- wordpress-0.2.0/recipes/mysql.rb
+++ wordpress-0.2.0/recipes/mysql.rb
@@ -46,7 +46,7 @@ if platform_family?("centos", "rhel")
     mysql_database_user 'wordpress_prod' do
       connection mysql_connection_info
       password       node['mysql']['server_user_password']
-      database_name 'wordress'
+      database_name 'wordpress'
       host          '%'
       privileges    [:select,:insert,:update,:delete,:create,:drop,:references,:index,:alter,:'create temporary tables',:'lock tables',:execute,:'create view',:'show view',:'create routine',:'alter routine',:trigger]
       action        [:create,:grant]
