01edd88dd45c9a384517cb39b9fa0c76252e3da6
melbit-waseem.rauf <waseem.rauf@melbourneit.com.au>
2014-09-23 20:44:02 +1000

    test

--- provisioning_linux/recipes/sftpjailroot.rb
+++ provisioning_linux/recipes/sftpjailroot.rb
@@ -6,15 +6,16 @@ if platform_family?("centos", "rhel")
 
 		user "waseemtest" do
 			action :create
+			supports :manage_home => true
 			system true
 			comment "Jail Root sFTP user"
-			home "/home/%u"
+			home '/home/%u'
 			shell "/sbin/nologin"
-			password "#{randompass}"
-			supports :manage_home => true 
+			password "#{randompass}" 
 		end
 
 			group "sftpusers" do
+			system true
 			action :create
 			members ['waseemtest']
 		end
