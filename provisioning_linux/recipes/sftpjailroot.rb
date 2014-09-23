83b06f7ec8f8123e576a218a86e5cd83e9faad40
melbit-waseem.rauf <waseem.rauf@melbourneit.com.au>
2014-09-23 21:10:42 +1000

    test

--- provisioning_linux/recipes/sftpjailroot.rb
+++ provisioning_linux/recipes/sftpjailroot.rb
@@ -9,15 +9,46 @@ if platform_family?("centos", "rhel")
 			supports :manage_home => true
 			system true
 			comment "Jail Root sFTP user"
-			home '/home/%u'
+			home '/home/waseemtest'
 			shell "/sbin/nologin"
 			password "#{randompass}" 
 		end
-
 			group "sftpusers" do
 			system true
 			action :create
-			members ['waseemtest']
+			members ['waseemtest','apache']
 		end
 
+		directory "/home/waseemtest/public_html" do
+			action :create
+			owner "root"
+			group "root"
+			mode "0755"
+			path "name"
+			recursive false
+		end
+
+		directory "/home/waseemtest" do
+			owner "waz"
+			group "sftpusers"
+			mode "0755"
+			path "name"
+			recursive true
+		end
+		
+		directory "/home/waseemtest" do
+			owner "root"
+			group "sftpusers"
+			mode "0770"
+			path "name"
+			recursive false
+		end
+
+		mount "/home/waseemtest/public_html" do
+			action [:mount, :enable]
+			mount_point "/var/www/html"
+			options "bind, rw"
+		end
+		
+		
 end	
