c8f88f069f9624df44b91c55b08f16457e114f40
melbit-waseem.rauf <waseem.rauf@melbourneit.com.au>
2014-09-24 12:03:21 +1000

    test

--- provisioning_linux/recipes/sftpjailroot.rb
+++ provisioning_linux/recipes/sftpjailroot.rb
@@ -54,10 +54,10 @@ if platform_family?("centos", "rhel")
 
 
 		conf_plain_file '/etc/ssh/sshd_config' do
-		  pattern		/Subsystem sftp  \/usr\/libexec\/openssh\/sftp-server/ 
-		  #current_line  'Subsystem sftp  /usr/libexec/openssh/sftp-server'
+		  #pattern		/Subsystem sftp  \/usr\/libexec\/openssh\/sftp-server/ 
+		  current_line  'Subsystem sftp  /usr/libexec/openssh/sftp-server'
 		  new_line 		'Subsystem sftp internal-sftp'
-		  action   :replace
+		  action :replace
 		end
 
 		mount "/var/www/html" do
