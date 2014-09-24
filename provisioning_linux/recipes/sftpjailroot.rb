2b886a3dcb8066724fbe10309ea6227351bc3a61
melbit-waseem.rauf <waseem.rauf@melbourneit.com.au>
2014-09-24 12:52:41 +1000

    test

--- provisioning_linux/recipes/sftpjailroot.rb
+++ provisioning_linux/recipes/sftpjailroot.rb
@@ -56,7 +56,7 @@ if platform_family?("centos", "rhel")
 		conf_plain_file '/etc/ssh/sshd_config' do
 		  pattern		/(Subsystem sftp \/usr\/libexec\/openssh\/sftp-server)/ 
 		  new_line 		'Subsystem sftp internal-sftp'
-		  action :insert_after_match
+		  action :replace
 		end
 
 		conf_plain_file '/etc/ssh/sshd_config' do
