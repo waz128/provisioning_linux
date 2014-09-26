e9580951f4b7b4442be224e83406f2dc03090254
unknown <waseem.rauf@WC057237.mit>
2014-09-26 22:51:58 +1000

    test

--- provisioning_linux/recipes/sftpjailroot.rb
+++ provisioning_linux/recipes/sftpjailroot.rb
@@ -48,8 +48,8 @@ if platform_family?("centos", "rhel")
 		end
 
 		conf_plain_file '/etc/ssh/sshd_config' do
-		  #current_line 'Subsystem sftp  /usr/libexec/openssh/sftp-server'
-		  pattern		/(Subsystem sftp \/usr\/libexec\/openssh\/sftp-server)/ 
+		  current_line 'Subsystem sftp /usr/libexec/openssh/sftp-server'
+		  #pattern		/(Subsystem sftp \/usr\/libexec\/openssh\/sftp-server)/ 
 		  new_line 'Subsystem sftp internal-sftp'
 		  action :replace
 		end
