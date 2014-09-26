751ed6dae3298b7ac9a6cb33f0b157ca97405ced
unknown <waseem.rauf@WC057237.mit>
2014-09-26 22:47:19 +1000

    test

--- provisioning_linux/recipes/sftpjailroot.rb
+++ provisioning_linux/recipes/sftpjailroot.rb
@@ -38,13 +38,6 @@ if platform_family?("centos", "rhel")
 			group "sftpusers"
 			mode "0755"
 		end
-
-		directory "/home/#{sftpuser}" do
-			owner "#{sftpuser}"
-			group "sftpusers"
-			mode "0755"
-			#recursive true
-		end
 		
 		directory "/home/#{sftpuser}" do
 			owner "root"
